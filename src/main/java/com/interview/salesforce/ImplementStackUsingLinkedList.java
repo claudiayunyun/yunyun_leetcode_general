48e564090657d6b07c1e9bc5708146a23c3eff4b
Yunyun Huang <claudiayunyun@gmail.com>
2021-01-10 11:09:45 -0800

    leetcode questions

---
+++ src/main/java/com/interview/salesforce/ImplementStackUsingLinkedList.java
@@ -0,0 +1,11 @@
+package com.interview.salesforce;
+
+
+/*
+ * 1 Implement a stack using single linked list concept
+ * 2 All the single linked list operations perform based 
+ *   on Stack operations LIFO(last in first out) 
+ */
+public class ImplementStackUsingLinkedList {
+
+}
